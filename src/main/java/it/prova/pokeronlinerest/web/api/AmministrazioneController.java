9cf5e61d044063d92eba460339c4fd5d48363e93
giacomomangelli <giacomomsidp@gmail.com>
2021-05-23 23:54:19 +0200

    un controller e mezzo utente e tavolo

---
+++ src/main/java/it/prova/pokeronlinerest/web/api/AmministrazioneController.java
@@ -0,0 +1,77 @@
+package it.prova.pokeronlinerest.web.api;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import it.prova.pokeronlinerest.model.Utente;
+import it.prova.pokeronlinerest.service.utente.UtenteService;
+import it.prova.pokeronlinerest.web.api.exception.UtenteNotFoundException;
+
+@RestController
+@RequestMapping("api/utente")
+public class AmministrazioneController {
+
+	@Autowired
+	private UtenteService utenteServiceInstance;
+
+	@GetMapping
+	public List<Utente> listAll() {
+		return utenteServiceInstance.listAllUtenti();
+	}
+
+	@GetMapping("/{id}")
+	public Utente findById(@PathVariable(value = "id", required = true) Long id) {
+		Utente utente = utenteServiceInstance.caricaSingoloUtenteEager(id);
+		if (utente == null) {
+			throw new UtenteNotFoundException("Utente not found with id: " + id);
+		}
+		return utente;
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Utente createNewUtente(@Valid @RequestBody Utente utenteInstance) {
+		return utenteServiceInstance.inserisci(utenteInstance);
+	}
+
+	@PutMapping("/{id}")
+	public Utente updateUtente(@PathVariable(value = "id", required = true) Long id,
+			@Valid @RequestBody Utente utenteInstance) {
+		Utente utente = utenteServiceInstance.caricaSingoloUtenteEager(id);
+		if (utente == null) {
+			throw new UtenteNotFoundException("Utente not found with id: " + id);
+		}
+		utenteInstance.setId(id);
+		return utenteServiceInstance.aggiorna(utenteInstance);
+
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public void deleteUtente(@PathVariable(value = "id", required = true) Long id) {
+		Utente utente = utenteServiceInstance.caricaSingoloUtente(id);
+		if (utente == null) {
+			throw new UtenteNotFoundException("Utente not found with id: " + id);
+		}
+		utenteServiceInstance.rimuovi(utente);
+	}
+
+	@PostMapping("/search")
+	public List<Utente> searchUtente(Utente utenteExample) {
+		return utenteServiceInstance.findByExample(utenteExample);
+	}
+
+}
--- src/main/java/it/prova/pokeronlinerest/web/api/GestioneTavoloController.java
+++ src/main/java/it/prova/pokeronlinerest/web/api/GestioneTavoloController.java
@@ -1,5 +1,36 @@
 package it.prova.pokeronlinerest.web.api;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import it.prova.pokeronlinerest.model.Tavolo;
+import it.prova.pokeronlinerest.service.tavolo.TavoloService;
+import it.prova.pokeronlinerest.web.api.exception.TavoloNotFoundException;
+
+@RestController
+@RequestMapping("api/tavolo")
 public class GestioneTavoloController {
+
+	@Autowired
+	private TavoloService tavoloServiceInstance;
+
+	@GetMapping
+	public List<Tavolo> listAllTavoli() {
+		return tavoloServiceInstance.listAllTavoli();
+	}
+
+	@GetMapping("/{id}")
+	public Tavolo findById(@PathVariable(value = "id", required = true) Long id) {
+		Tavolo tavolo = tavoloServiceInstance.caricaSingoloTavoloEager(id);
+		if (tavolo == null) {
+			throw new TavoloNotFoundException("Tavolo not found " + id);
+		}
+		return tavolo;
+	}
 
 }
